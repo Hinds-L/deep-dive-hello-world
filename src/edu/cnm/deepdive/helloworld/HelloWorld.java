a17576397f4113030a8ca23677cf4381001f4498
Leslie <leslie.hinds82@gmail.com>
2018-09-02 09:35:00 -0600

    New project files, with file implementing ("Hello World") program.

---
+++ src/edu/cnm/deepdive/helloworld/HelloWorld.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package edu.cnm.deepdive.helloworld;
+
+/**
+ * Class that displays the "Hello World!" message.
+ *
+ */
+public class HelloWorld {
+
+  /**
+   * Prints "Hello World!" to output.
+   *    * @param args
+   */
+  public static void main(String[] args) {
+   System.out.lnprint("Hello World!");
+  }
+
+}
